aaf8843d6d63034e3582e844bbe3778a5907076f
Ashley Baird <AshleyBaird@Ashleys-MBP.home>
2015-06-24 23:57:20 -0400

    finished the project

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,10 +1,14 @@
 ////////////////////////////////////////////////
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
-'use strict';
 
+
+////////////////////////////////////////////////
+/*   Provided Code - Please Don't Edit   */
+////////////////////////////////////////////////
+playToFive();
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
+    console.log("Please choose either 'rock', 'paper', or 'scissors'.");
     return prompt();
 }
 function randomPlay() {
@@ -17,39 +21,71 @@ function randomPlay() {
         return "scissors";
     }
 }
+
 ////////////////////////////////////////////////
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return (move || getInput());
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return (move || randomPlay());
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
+    if (playerMove === computerMove) {
+        return "tie";
+    }
+    if (playerMove === "rock") {
+        if ( computerMove === "paper") {
+            return "computer";
+        } else {
+            return "player";
+        }
+    }
+    if (playerMove === "paper") {
+        if (computerMove === "rock") {
+            return "player";
+        } else {
+            return "computer";
+        }
+    }    
+    if (playerMove === "scissors") {
+        if (computerMove === "rock") {
+            return "computer";
+        } else {
+            return "player";
+        }
+    }
 }
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+    var gameNotWon = true;
+    while (gameNotWon) {
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins);
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner(playerMove,computerMove);
+        
+        if (winner == "player"){
+            console.log("Player played '" + playerMove + "' and Computer played '" + computerMove + "', the winner is Player!");
+            playerWins++;
+        }
+        else if (winner == "computer"){
+            console.log("Player played '" + playerMove + "' and Computer played '" + computerMove + "', the winner is Computer!");
+            computerWins ++;
+        }
+          else if (winner == "tie"){
+            console.log("Player played '" + playerMove + "' and Computer played '" + computerMove + "', it's a tie!");
+        }
+        if (playerWins >= 5 || computerWins >= 5){
+            gameNotWon = false;
+        }
+    }
+  return [playerWins, computerWins];
 }
-
